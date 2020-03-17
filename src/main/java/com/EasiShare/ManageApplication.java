16c15fc58a9658db08d8026b6d35d301343703da
AnatoliyPetuhov <anatoliy.p@inspire-tech.com>
2020-03-17 08:36:09 +0700

    update yml

--- src/main/java/com/EasiShare/ManageApplication.java
+++ src/main/java/com/EasiShare/ManageApplication.java
@@ -1,9 +1,10 @@
 package com.EasiShare;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties
+@SpringBootApplication
 public class ManageApplication {
 
     public static void main(String[] args) {
